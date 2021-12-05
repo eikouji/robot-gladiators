f9f16584851378bbdc62f4fe87d4edb7dc86a816
Eiko ujifusa <eikorouji@gmail.com>
2021-12-05 09:31:38 -0800

    messing around with console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,31 @@
 
 
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot champion's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 30 // 
+console.log(10 + 20);
+// what is this? // 
+console.log("Our robot's name is " + playerName);
+// Tony, the Robot, is ready for battle // 
+console.log("Tony the Robot " + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!" // 
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!" // 
+console.log("Your robot, " + playerName + ", has won!");
+
+
+
+
+// this creates a function named "fight" //
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+
+
+
+
+// fight();
